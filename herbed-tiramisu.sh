f0011a473ce65474d5410adab7ee88fe3801e116
mertoalex <baytemurmuslum+github@gmail.com>
2023-04-16 22:34:32 +0000

    wth

--- herbed-tiramisu.sh
+++ herbed-tiramisu.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+#thanks to anufrievroman for style (https://github.com/anufrievroman/polytiramisu/blob/9c0a039d8cd8b7066bccbbd237cd8939da66e1fb/polytiramisu.sh#L18)
 tiramisu -o "export timeout=#timeout app=#source message=#summary" |
+
 while read -r tiramisu
 do	$tiramisu
 timeout -s9 ${timeout}s herbe "App: $app" "" "Message: $message"
